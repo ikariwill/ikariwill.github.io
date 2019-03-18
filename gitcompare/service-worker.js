91493870956865601794bd9862fb1a4e28e5d3f9
Willian Home <willian@wiki4fit.com.br>
2019-03-18 02:50:20 -0300

    Release

--- gitcompare/service-worker.js
+++ gitcompare/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18240e00652393a032f26aac35af6ef9.js"
+  "/gitcompare/precache-manifest.79c0ff509065376d41d24eef34fa1521.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/gitcompare/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
