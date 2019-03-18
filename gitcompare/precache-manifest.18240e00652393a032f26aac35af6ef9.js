1ed7d7ede8ff4d2350acbd9a39c568a5282c1f1f
Willian Home <willian@wiki4fit.com.br>
2019-03-18 02:36:27 -0300

    firts build

---
+++ gitcompare/precache-manifest.18240e00652393a032f26aac35af6ef9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "9063717bbff148b1ee59",
+    "url": "/static/js/main.d61878f1.chunk.js"
+  },
+  {
+    "revision": "b0f767e462de0961efd2",
+    "url": "/static/js/2.e86fa9a7.chunk.js"
+  },
+  {
+    "revision": "b0f767e462de0961efd2",
+    "url": "/static/css/2.e56e71af.chunk.css"
+  },
+  {
+    "revision": "c94c19b9cfc4bfffe41254b986801625",
+    "url": "/index.html"
+  }
+];
